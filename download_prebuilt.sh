e75612a9a0036fffdfdaa28fa3289dff425ca5a1
Maju <maju@majudev.net>
2016-11-28 22:35:10 +0100

    Added prebuilt windows binaries download script

---
+++ download_prebuilt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Downloading prebuilt-windows-x64..."
+wget http://files.majudev.net/Wicher/prebuilt_windows_x64.tar.xz
+echo "Downloading prebuilt-windows-x86..."
+wget http://files.majudev.net/Wicher/prebuilt_windows_x86.tar.xz
